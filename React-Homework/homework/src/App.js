4bc5f142ef1686e8fd7acc63f0f1cae3d6deb7fc
r8yt <hendersonkevin64@gmail.com>
2019-06-12 10:44:29 -0500

    score is being kept for both points

--- React-Homework/homework/src/App.js
+++ React-Homework/homework/src/App.js
@@ -10,27 +10,23 @@ import Spacer from "./components/Spacer"
 class App extends React.Component {
   state = {
     images,
-    count : 0
-    
+    count : 1,
+    score: [0]
   };
  
   handleIncrement = () => {
-    // We always use the setState method to update a component's state
+    
+    
     this.setState({ count: this.state.count + 1 });
     console.log(this.state.count);
+    this.state.score.push(this.state.count);
+    console.log(this.state.score);
     
   };
 
-  handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    let id = event.target.id;
-    const count = event.target.count;
-
-    console.log(id)
-    console.log(count);
-  };
 
   render() {  
+
     var shuffle = function (array) {
 
       var currentIndex = array.length;
@@ -53,8 +49,6 @@ class App extends React.Component {
     };
   
     shuffle(images);
-
-    
     
     return (
       <div>
@@ -63,7 +57,7 @@ class App extends React.Component {
         <ul className="navbar">
      <li> <a href="/">Clickster</a></li>
      <li>Click the image to begin!</li>
-     <li>Score: {this.state.count} | Top Score: 0</li>
+     <li>Score: {this.state.count - 1} | Top Score: {Math.max.apply(null, this.state.score)}</li>
      </ul> 
     
         <Spacer />
