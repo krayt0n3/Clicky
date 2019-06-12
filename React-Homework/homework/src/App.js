a2bc9c6f2da1c6c0d0d7d39a74b8a5939b0e1642
r8yt <hendersonkevin64@gmail.com>
2019-06-12 09:30:03 -0500

    added shuffle functionality

--- React-Homework/homework/src/App.js
+++ React-Homework/homework/src/App.js
@@ -11,17 +11,51 @@ class App extends React.Component {
   state = {
     images,
     count : 0
+    
   };
  
   handleIncrement = () => {
     // We always use the setState method to update a component's state
     this.setState({ count: this.state.count + 1 });
     console.log(this.state.count);
+    
   };
 
-  render() {
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let id = event.target.id;
+    const count = event.target.count;
 
+    console.log(id)
+    console.log(count);
+  };
+
+  render() {  
+    var shuffle = function (array) {
+
+      var currentIndex = array.length;
+      var temporaryValue, randomIndex;
+    
+      
+      while (0 !== currentIndex) {
+        
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+    
+      return array;
+    
+    };
   
+    shuffle(images);
+
+    
+    
     return (
       <div>
         
@@ -43,6 +77,7 @@ class App extends React.Component {
             name={image.name}
             image={image.image}
             handleIncrement={this.handleIncrement}
+            onChange={this.handleInputChange}
             
           />
         ))}
